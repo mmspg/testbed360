76d50cb42b3f538621fde0e77811addf1efb9b52
Evgeniy Upenik <evgeniy.upenik@epfl.ch>
2017-02-10 12:39:18 +0100

    Copyright and lisence update

--- ios/Testbed360/Testbed360/AppDelegate.swift
+++ ios/Testbed360/Testbed360/AppDelegate.swift
@@ -1,10 +1,26 @@
 //
 //  AppDelegate.swift
-//  mmspg360
+//  Testbed360
 //
 //  Created by Evgeniy Upenik on 08/06/16.
-//  Copyright © 2016 Evgeniy Upenik. All rights reserved.
 //
+//  Copyright (C) 2017 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland
+//  Multimedia Signal Processing Group
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 import UIKit
 
